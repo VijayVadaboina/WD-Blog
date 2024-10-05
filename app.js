06cd22e5f5f50e342ba949f0c71aaf6d4864eca8
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-10-05 15:57:50 +0530

    added db connection & schema

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require("./server/config/db");
+
 const app = express();
 const PORT = process.env.PORT || 3000; //process.env.PORT should be first arg as its a truthy value evaluation, otherwise it shouldnt consider it
 
+connectDB();
 app.use(express.static("public"));
 //Template engine setup
 app.use(expressLayout);
