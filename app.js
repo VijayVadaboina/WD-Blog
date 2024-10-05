a0b0fcdb32315fd8b98a288171f64b409e7cafb0
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-10-05 09:27:35 +0530

    adding app.js & routes initial structure

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+
+const app = express();
+const PORT = process.env.PORT || 3000; //process.env.PORT should be first arg as its a truthy value evaluation, otherwise it shouldnt consider it
+
+app.use(express.static("public"));
+//Template engine setup
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+
+app.listen(3000, () => {
+  console.log(`APP started listening on port ${PORT}`);
+});
